3ac48e9f651b4135233d20ea1e6812e7688e401c
MattLuo90 <mattluo604@hotmail.com>
2021-07-19 16:32:39 -0700

    add state for categroy pick

--- App.js
+++ App.js
@@ -12,11 +12,25 @@ import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false)
-  return <Screen>
-    <AppPicker icon="apps" placeholder="Category" />
-    <AppTextInput icon="email" placeholder="Email" />
-  </Screen>
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
