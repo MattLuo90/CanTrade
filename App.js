95c7496e1cb228047bad49aaef3ade99dad14e2b
MattLuo90 <mattluo604@hotmail.com>
2021-07-19 11:38:36 -0700

    add list detail screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View } from "react-native";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
