e2b499ee028dd94f18b4c1637152e80144c93dc9
MattLuo90 <mattluo604@hotmail.com>
2021-07-19 15:00:09 -0700

    add delete component

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { View } from "react-native";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
