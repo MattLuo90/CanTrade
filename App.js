d4a1e09277efab01d53a84c2bf2816ea08cc8f7e
MattLuo90 <mattluo604@hotmail.com>
2021-07-19 11:23:37 -0700

    add APP card component

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
