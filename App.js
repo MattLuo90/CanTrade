0b0bcbe2ebbf83a6cc9504d5f8d2faf98211eef5
MattLuo90 <mattluo604@hotmail.com>
2021-07-26 17:03:42 -0700

    add navigation feature

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Switch, TextInput, View, Button, Image } from "react-native";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/lists/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import * as ImagePicker from 'expo-image-picker'
-import * as Permissions from 'expo-permissions'
-import ImageInput from "./app/components/ImageInput";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import ImageInputList from "./app/components/ImageInputList";
+import React from "react";
 
-
+import { NavigationContainer } from "@react-navigation/native";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
-
-
   return (
-    <ListingEditScreen />
-  )
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
+
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
-import AppButton from "../components/AppButton";
 
-function WelcomeScreen(props) {
+import Button from "../components/Button";
+import routes from "../navigation/routes";
+
+function WelcomeScreen({ navigation }) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
-      blurRadius={10}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
         <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
       <View style={styles.buttonsContainer}>
-        <AppButton title="Login" onPress={() => console.log('tapped')}></AppButton>
-        <AppButton title="Register" color="secondary" onPress={() => console.log('tapped')}></AppButton>
+        <Button
+          title="Login"
+          onPress={() => navigation.navigate(routes.LOGIN)}
+        />
+        <Button
+          title="Register"
+          color="secondary"
+          onPress={() => navigation.navigate(routes.REGISTER)}
+        />
       </View>
-    </ImageBackground >
+    </ImageBackground>
   );
 }
 
@@ -29,7 +38,7 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     padding: 20,
-    width: '100%',
+    width: "100%",
   },
   logo: {
     width: 100,
@@ -40,16 +49,11 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: "center",
   },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
-  },
   tagline: {
     fontSize: 25,
-    fontWeight: '600',
+    fontWeight: "600",
     paddingVertical: 20,
-  }
+  },
 });
 
 export default WelcomeScreen;
