b072ad4027d98a330caeeaa15d29fb9e6fb56dde
MattLuo90 <mattluo604@hotmail.com>
2021-09-14 16:42:03 -0700

    add offline support

--- React-Native-Front-End/App.js
+++ React-Native-Front-End/App.js
@@ -4,26 +4,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function App() {
-  const demo = async (value) => {
-    try {
-      await AsyncStorage.setItem('@storage_Key', JSON.stringify({ id: 1 }))
-      const value = await AsyncStorage.getItem('@storage_Key')
-      const person = JSON.parse(value)
-      console.log(person)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-  demo();
-  return null
-  // (
-  //   <NavigationContainer theme={navigationTheme}>
-  //     <AppNavigator />
-  //   </NavigationContainer>
-  // );
+
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
 
