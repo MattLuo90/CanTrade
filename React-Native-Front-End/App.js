9bd398dae50c4fa35aa38e1a956877949707dbd1
MattLuo90 <mattluo604@hotmail.com>
2021-09-15 16:22:33 -0700

    add user authentication

--- React-Native-Front-End/App.js
+++ React-Native-Front-End/App.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
+import AppLoading from 'expo-app-loading';
 
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator"
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
 
 
 export default function App() {
+  const [user, setUser] = useState()
+  const [isReady, setIsReady] = useState(false)
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken();
+
+    if (!token) return;
+
+    setUser(jwtDecode(token))
+  }
+
+  if (!isReady) {
+    return (
+      <AppLoading startAsync={() => restoreToken()} onFinish={() => setIsReady(true)} />
+    );
+  }
 
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
 
